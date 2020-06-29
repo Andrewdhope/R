6dde553d59f03595792adcfdf9128bd501aac079
Andrew Hope <andrewdhope@gmail.com>
2020-06-28 20:02:29 -0700

    initial draft

--- apportionment/apportionment_functions.R
+++ apportionment/apportionment_functions.R
@@ -3,6 +3,7 @@
 # API Documentation: https://www.census.gov/data/developers/geography.html <<< INACCURATE
 # API Example: 
 # http://api.census.gov/data/2010/dec/sf1?key=9d57165f0c02abba2b8838cc2deedc830e271035&get=P001001&for=state:06
+# Method of Equal Proportions: https://census.gov/population/apportionment/about/computing.html
 #
 # ---------- ---------- ----------
 #             Title
@@ -10,7 +11,50 @@
 # 
 # description 
 #
-# /// --- getCensusDataframe --- ///
+buildDataframe <- function(use_limit = FALSE) {
+    
+    df <- getCensusData()
+    df <- as.data.frame(df)
+    
+    # move variable names into colnames
+    colnames(df) <- df[1,]
+    df <- df[-1,]
+    df <- df[,-4]
+    
+    # set data types
+    df$P001001 <- as.numeric(df$P001001)
+    
+    # set seat limit
+    seat_limit <- 435
+    if (use_limit) {
+        total_population <- sum(df$P001001)
+        seat_limit <- proposedCount(total_population, 30000, 100)    
+    }
+    
+    # instantiate seats
+    df <- cbind(df, seats = 1)
+    
+    # instantiate multiplier
+    multiplier <- recalculateMultiplier(df)
+    df <- cbind(df, multiplier = multiplier)
+    
+    # loop to allocate seats
+    while (sum(df$seats) < seat_limit) {
+        multiplier <- recalculateMultiplier(df)
+        df$multiplier = multiplier
+        df$seats[which.max(df$multiplier)] = df$seats[which.max(df$multiplier)] + 1
+    }
+    
+    df
+}
+
+recalculateMultiplier <- function(df) {
+    geometric_mean <- (1/(sqrt(df$seats+1)*df$seats))
+    multiplier <- df$P001001 * geometric_mean
+    multiplier
+}
+
+# /// --- getCensusData --- ///
 # 
 # Notes:
 #   Use httr and jsonlite
@@ -18,10 +62,10 @@
 # Next step:
 #   Read through getCensus and decide whether to us it or to use httr and jsonlite
 #
-getCensusDataframe <- function() {
+getCensusData <- function() {
   library('httr')
   library('jsonlite')
-  library('censusapi') # getreq <- getCensus("dec/sf1", vintage = 2010, key, vars = c("P001001"), region = "state:06")
+  # getreq <- censusapi::getCensus("dec/sf1", vintage = 2010, key, vars = c("P001001"), region = "state:06")
   url <- "https://api.census.gov/data/2010/dec/sf1"
   key <- "9d57165f0c02abba2b8838cc2deedc830e271035"
   # build state:01,02,03...56
@@ -35,31 +79,26 @@ getCensusDataframe <- function() {
   }
   req_httr <- httr::GET(url, query = list(key = "9d57165f0c02abba2b8838cc2deedc830e271035", get = "STATE,LSAD_NAME,P001001", "for" = states_string))
   req_content <- httr::content(req_httr, as = "text")
-  df <- jsonlite::fromJSON(req_content)
-  
-  # move variable names into colnames
-  colnames(df) <- df[1,] 
-  df <- df[-1,]
-  
-  print(df)
-  df
+  raw_json <- jsonlite::fromJSON(req_content)
+  raw_json
 }
+
 # /// --- proposedCount --- ///
 # p - total population
 # i - initial multiplier, multiplied by 10,000 (typically 3 for a ration of 1 per 30,000) 
 # x - step value, number of representatives to increment to the next step of the function
 #
-proposedCount <- function(population,init,stepval){
-  reps <- 0
-  while (TRUE) {
-    if ((population/(10000*init))>=stepval) {
-      reps <- reps+stepval
-      population <- population-(10000*init*stepval)
-    } else {
-        reps <- reps + ceiling(population/(10000*init))
-        break
+proposedCount <- function(population, init = 30000, stepval = 100){
+    seat_limit <- 0
+      while (TRUE) {
+        if ((population/(10000*init))>=stepval) {
+          seat_limit <- seat_limit+stepval
+          population <- population-(10000*init*stepval)
+        } else {
+            seat_limit <- seat_limit + ceiling(population/(10000*init))
+            break
+          }
+        init <- init+1
       }
-    init <- init+1
-  }
-  reps
+    seat_limit
 }
